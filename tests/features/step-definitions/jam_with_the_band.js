88d881ca7aa6045a5dfccb17fd4cd3fb84470751
SaraSorlin <sorlin82@gmail.com>
2024-11-06 12:56:44 +0100

    test works

--- tests/features/step-definitions/jam_with_the_band.js
+++ tests/features/step-definitions/jam_with_the_band.js
@@ -4,23 +4,10 @@ import { expect } from 'chai';
 import { navigateTo, getWhereIAm, getMenuChoiceElement, checkIfDescriptionContainsString, cheatIfNeeded } from './helpers.js'
 
 
-Given('that I have started the game by navigating to {string}', async function (url) {
 
-  await this.gotoUrl(url);
-
-});
-Given('the user travel to location {string}', async function (toLocation) {
-  await navigateTo(this, toLocation);
-});
-Given('that the user is located {string}', async function (position) {
-  expect(await getWhereIAm(this)).to.equal(position);
-
-
-});
 
 When('the user wait long enough until this text display {string}', async function (textDisplay) {
   while (!await checkIfDescriptionContainsString(this, textDisplay, true)) {
-
     await cheatIfNeeded(this);
 
     let waitButton = await getMenuChoiceElement(this, 'Wait');
@@ -28,6 +15,8 @@ When('the user wait long enough until this text display {string}', async functio
   }
 
 });
+
+
 
 Then('the user should have a choice to {string}', async function (JamWithTheBand) {
   let JamButton = await getMenuChoiceElement(this, JamWithTheBand);
